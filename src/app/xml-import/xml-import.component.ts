12eb6bd9f76a0b2b4dcb133dc8dff6db4b916667
mukund.s@intelaccounts.com <mukund.s@intelaccounts.com>
2021-06-05 11:36:00 +0530

    changes for read xml|main

--- src/app/xml-import/xml-import.component.ts
+++ src/app/xml-import/xml-import.component.ts
@@ -1,11 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import * as xml2js from 'xml2js';
-const xmlString = `
-  <foo:EntryItem>
-    <foo:Id>1</foo:Id>
-    <foo:Name>One</foo:Name>
-  </foo:EntryItem>
-`;
+import * as converter from 'xml-js';
 @Component({
   selector: 'app-xml-import',
   templateUrl: './xml-import.component.html',
@@ -14,9 +8,11 @@ const xmlString = `
 
 export class XmlImportComponent implements OnInit {
   xml: string;
+  outputXml: any;
+  inputXml: any;
+  list: any = []
+  constructor() {
 
-  constructor() { 
-   
   }
 
   ngOnInit() {
@@ -27,35 +23,58 @@ export class XmlImportComponent implements OnInit {
     return name.substring(0, name.lastIndexOf('.'));
   }
   getFileExtension(filename) {
-    return (filename!=''&&filename!=undefined&&filename!=null)?filename.split('.').pop():'';
+    return (filename != '' && filename != undefined && filename != null) ? filename.split('.').pop() : '';
   }
-  fileType: any = ''
-  fileName: any = '';
-  file: any = '';
- readThis(inputValue: any): void {
-    var file: File = inputValue.files[0];
+  selectFile(event) {
+    var file: File = event.target.files[0];
     this.fileType = this.getFileExtension(file.name);
     this.fileName = this.getFilename(file.name);
     this.file = file.name;
 
 
-    if (this.fileType!='xml') {
+    if (this.fileType != 'xml') {
       alert("Wrong XML");
       return;
     }
-    var myReader: FileReader = new FileReader();
-    myReader.onloadend = (e) => {
-      this.uploadedFile = myReader.result;
-      this.ReadNow();
+    const reader = new FileReader();
+    reader.onload = (e: any) => {
+      let xml = e.target.result;
+      this.inputXml = xml;
+      let result1 = converter.xml2json(xml, { compact: true, spaces: 2 });
+
+      const JSONData = JSON.parse(result1);
+      this.xml = JSONData;
+      this.list = [];
+      let list: any = [];
+      list = Array.isArray(JSONData.employees.employee) ? JSONData.employees.employee : [];
+      let index:number=0;
+      if (list) {
+        for (let i of list) {
+          i.class_name='info';
+          if(index%2==0)
+          {
+            i.class_name='success';
+          }
+          let t: any = {
+            class_name:i.class_name?i.class_name:'',
+            id: i._attributes.id ? i._attributes.id : '',
+            name: i.name._text ? i.name._text : '',
+            email: i.email._text ? i.email._text : '',
+            mobile: i.mobile._text ? i.mobile._text : '',
+          }
+          this.list.push(t)
+          index++;
+        }
+      }
+
+
+
     }
-    myReader.readAsDataURL(file);
+
+    reader.readAsText(event.target.files[0])
   }
-  ReadNow()
-  {
-    console.log(this.uploadedFile);
-    const parser = new xml2js.Parser({ strict: false, trim: true });
-    parser.parseString(this.uploadedFile, (err, result) => {
-      this.xml = result;
-    });
-  }
+  fileType: any = ''
+  fileName: any = '';
+  file: any = '';
+
 }
